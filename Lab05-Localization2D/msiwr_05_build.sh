7959e1b33ad2f96778e5659413b92bb6c35240c5
michalnowicki <michal.nowicki@put.poznan.pl>
2021-12-20 00:05:47 +0100

    Removing old lab1011

--- Lab05-Localization2D/msiwr_05_build.sh
+++ Lab05-Localization2D/msiwr_05_build.sh
@@ -2,5 +2,4 @@ xhost +local:root
 
 # BUILD THE IMAGE
 ROS_IMAGE="msiwr/image05"
-ROS_CONTAINER="MSIwR_05"
 docker build -f Dockerfile -t $ROS_IMAGE ./..
--- Lab10-Semantic/segmentation_models.pytorch/msiwr_10_build_cpu.sh
+++
@@ -1,5 +0,0 @@
-xhost +local:root
-
-# BUILD THE IMAGE
-ROS_IMAGE="msiwr/image10cpu"
-docker build -f docker/DockerfileCPU -t $ROS_IMAGE .
